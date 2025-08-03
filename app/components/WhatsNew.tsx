27e74755359ec91e42840ff3d5513db52000ce57
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-02 21:50:22 -0700

    Revert "Merge branch 'test'"
    This reverts commit 8d39617fbf9f62ca1e0e7d179e8bee4ec168bdb4, reversing
    changes made to 8ec6d065975ec2fc81df86cbbc0f3ef71118b9ef.

--- app/components/WhatsNew.tsx
+++ app/components/WhatsNew.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import BigDaisyDisplayCard from './BigDaisyDisplayCard'
-import websc from "../assets/smartsnap_8-2-2025_at_21-21-15 (1).png"
 
 const WhatsNew = (): React.ReactNode => {
     return (
         <div className='h-[80vh] bg-neutral flex items-center justify-center gap-8 '>
-            <BigDaisyDisplayCard image={websc} title='Welcome to our new website' text='This will be some sort of information.'></BigDaisyDisplayCard>
-            {/* <BigDaisyDisplayCard></BigDaisyDisplayCard> */}
+            <BigDaisyDisplayCard></BigDaisyDisplayCard>
+            <BigDaisyDisplayCard></BigDaisyDisplayCard>
         </div>
     )
 }
