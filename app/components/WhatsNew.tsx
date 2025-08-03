a75568779e05156ba5e594e6af2d4f0e8b474629
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-02 21:33:53 -0700

    Update WhatsNew.tsx

--- app/components/WhatsNew.tsx
+++ app/components/WhatsNew.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import BigDaisyDisplayCard from './BigDaisyDisplayCard'
+import websc from "../assets/smartsnap_8-2-2025_at_21-21-15 (1).png"
 
 const WhatsNew = (): React.ReactNode => {
     return (
         <div className='h-[80vh] bg-neutral flex items-center justify-center gap-8 '>
-            <BigDaisyDisplayCard></BigDaisyDisplayCard>
-            <BigDaisyDisplayCard></BigDaisyDisplayCard>
+            <BigDaisyDisplayCard image={websc} title='Welcome to our new website' text='This will be some sort of information.'></BigDaisyDisplayCard>
+            {/* <BigDaisyDisplayCard></BigDaisyDisplayCard> */}
         </div>
     )
 }
