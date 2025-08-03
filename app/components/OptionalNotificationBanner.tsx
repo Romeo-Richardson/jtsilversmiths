5a67eed0196358f2e92e52f86459d1daf0b3730a
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-02 21:01:20 -0700

    Update OptionalNotificationBanner.tsx

--- app/components/OptionalNotificationBanner.tsx
+++ app/components/OptionalNotificationBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OptionalNotificationBanner = (): React.ReactNode => {
     return (
         <div className='bg-black text-white flex items-center justify-center h-12'>
-            <p>Some sort of notification</p>
+            <a href='www.liveyounger.com/josefinatellechea'>Learn more about Lifewave's X39 regenerative phototherapeutic patches!</a>
         </div>
     )
 }
