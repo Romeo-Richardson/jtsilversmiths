4d00a273333a2c492766aa1b51fb32bc91325397
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-02 20:21:11 -0700

    change for deploy
    url changes

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const getItems = async () => {
-    const { data } = await axios.get("https://jtsilversmiths-t5l8.vercel.app/api/get-items")
+    const { data } = await axios.get("https://jtsilversmiths.com/api/get-items")
     return data
 }
