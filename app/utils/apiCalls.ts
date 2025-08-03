4c93a9ca154a8624bcdbc52e63d8894d58ceeb87
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-03 12:47:04 -0700

    Update apiCalls.ts

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const getItems = async () => {
-    const { data } = await axios.get("http://localhost:3000/api/get-items")
+    const { data } = await axios.get("https://www.jtsilversmiths.com/api/get-items")
     return data
 }
