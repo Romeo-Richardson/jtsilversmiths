ec7bab1973fa9ebd7d387d50ec5da2be5e3fece2
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-08-04 20:14:36 -0700

    Revert "update"
    This reverts commit a753efe5552e9479bebfab7bc0fe076a191b8617.

--- app/utils/TanstackProvider.tsx
+++ app/utils/TanstackProvider.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 import React, { useState } from 'react'
-import { Toaster } from 'react-hot-toast'
 
 const TanstackProvider = ({ children }: { children: any }) => {
 
@@ -12,9 +11,7 @@ const TanstackProvider = ({ children }: { children: any }) => {
     return (
         <>
             <QueryClientProvider client={client}>
-                <Toaster>
-                    {children}
-                </Toaster>
+                {children}
             </QueryClientProvider>
         </>
     )
