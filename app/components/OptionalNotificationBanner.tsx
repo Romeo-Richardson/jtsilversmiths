3ad34a2e758c29aa5c4ebf3115a537c1771d12e5
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2025-12-21 19:26:22 -0800

    updated banner text

--- app/components/OptionalNotificationBanner.tsx
+++ app/components/OptionalNotificationBanner.tsx
@@ -4,7 +4,7 @@ const OptionalNotificationBanner = (): React.ReactNode => {
     return (
         <div className='bg-black max-[700px]:px-6 max-[390px]:text-xs text-white flex items-center justify-center h-12'>
             <span>
-                <a href='https://liveyounger.com/josefinatellechea'>Look and feel young again</a>
+                <a href='https://liveyounger.com/josefinatellechea'>Look and feel young again! </a>
                 <a href="https://liveyounger.com/josefinatellechea" className='underline'>Click to learn more</a>
             </span>
 
