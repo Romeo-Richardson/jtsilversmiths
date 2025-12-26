df9ee5524fb129b6ebf335926d20fc96de20a723
Romeo Richardson <romeorichardson@Romeos-Mac-mini.localdomain>
2025-12-26 12:10:33 -0800

    Update route.ts

--- app/api/fix-themes/route.ts
+++ app/api/fix-themes/route.ts
@@ -2,20 +2,21 @@ import prisma from "@/prisma"
 import { NextRequest, NextResponse } from "next/server"
 
 
-export const GET = async ( req:NextRequest ) => {
+export const GET = async (req: NextRequest) => {
     try {
         await prisma.$connect()
         const allItems = await prisma.items.findMany()
-        if(!allItems){
-            return NextResponse.json({ error: "Failed to get item"}, {status: 500})
+        if (!allItems) {
+            return NextResponse.json({ error: "Failed to get item" }, { status: 500 })
         }
         allItems.forEach(async (item) => {
             console.log(item)
-            await prisma.items.update({where: {name: item.name}, data: { categories: {push: item.asIsTheme!}}})
-            await prisma.items.update({where: {name: item.name}, data: { categories: {push: item.asIsStyle!}}})
+            if (item.categories.includes("Heart")) {
+                await prisma.items.update({ where: { name: item.name }, data: { categories: { push: "Hearts" } } })
+            }
         })
-        return NextResponse.json({success: true}, {status: 200})
+        return NextResponse.json({ success: true }, { status: 200 })
     } catch (error) {
-        return NextResponse.json({error}, {status: 500})
+        return NextResponse.json({ error }, { status: 500 })
     }
 }
