b43b232b4f2dc665c580fe46b481ce773fb99c1a
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2026-01-01 16:43:09 -0800

    Updated style text
    For Bosals

--- app/api/fix-themes/route.ts
+++ app/api/fix-themes/route.ts
@@ -9,23 +9,7 @@ export const GET = async (req: NextRequest) => {
         if (!allItems) {
             return NextResponse.json({ error: "Failed to get item" }, { status: 500 })
         }
-        allItems.forEach(async (item) => {
-<<<<<<< Updated upstream
-            if (item.categories.includes("Diamter 1/2") && item.categories.includes("Latigo")) {
-                await prisma.items.update({ where: { name: item.name }, data: { asIsSize: "~ 11 1/2 x 5 1/2" } })
-=======
-            console.log(item)
-            // if (item.categories.includes("Bosalitas")) {
-            //     await prisma.items.update({ where: { name: item.name }, data: { categories: { push: "Diameter 1/4" } } })
-            // }
-            if (item.asIsSize?.includes("(inside measurement)")) {
-                await prisma.items.update({ where: { name: item.name }, data: { asIsSize: "" } })
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-            }
-        })
+        allItems.forEach(async (item) => { })
         return NextResponse.json({ success: true }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 })
