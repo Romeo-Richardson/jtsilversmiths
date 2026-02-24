65d0102e6cf0c6d5a0f8ac57a779ff2ee4d24be2
Romeo Richardson <romeorichardson@Romeos-Mac-mini.localdomain>
2026-02-24 10:54:33 -0800

    Update SideMenu.tsx

--- app/components/SideMenu.tsx
+++ app/components/SideMenu.tsx
@@ -505,6 +505,103 @@ const SideMenu = () => {
             ]
         },
         {
+            name: "Horse Hair",
+            link: "/",
+            items: [
+                {
+                    name: "Hat Bands",
+                    link: "/",
+                    subItems: [
+                        {
+                            name: "2 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "3 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "4 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "5 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "6 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "7 Strands",
+                            link: "/",
+                        },
+                    ]
+                },
+                {
+                    name: "Cinch",
+                    link: "/",
+                },
+                {
+                    name: "Fiador",
+                    link: "/",
+                },
+                {
+                    name: "Get Down Ropes",
+                    link: "/",
+                    subItems: [
+                        {
+                            name: "16 Feet",
+                            link: "/",
+                        },
+                        {
+                            name: "14 Feet",
+                            link: "/",
+                        },
+                    ]
+                },
+                {
+                    name: "Key Chains",
+                    link: "/",
+                },
+                {
+                    name: "Jewlery",
+                    link: "/",
+                    subItems: [
+                        {
+                            name: "Bracelets",
+                            link: "/",
+                        },
+                        {
+                            name: "Earrings",
+                            link: "/",
+                        },
+                        {
+                            name: "Sets",
+                            link: "/",
+                        },
+                    ]
+                },
+                {
+                    name: "Mecates (Mane Horse Hair)",
+                    link: "/",
+                    subItems: [
+                        {
+                            name: "4 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "6 Strands",
+                            link: "/",
+                        },
+                        {
+                            name: "8 Strands",
+                            link: "/",
+                        },
+                    ]
+                },]
+        },
+        {
             name: "Spurs",
             link: "/",
             items: [
@@ -651,103 +748,7 @@ const SideMenu = () => {
                 },
             ]
         },
-        // {
-        //     name: "Horse Hair",
-        //     link: "/",
-        //     items: [
-        //         {
-        //             name: "Hat Bands",
-        //             link: "/",
-        //             subItems: [
-        //                 {
-        //                     name: "2 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "3 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "4 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "5 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "6 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "7 Strands",
-        //                     link: "/",
-        //                 },
-        //             ]
-        //         },
-        //         {
-        //             name: "Cinch",
-        //             link: "/",
-        //         },
-        //         {
-        //             name: "Fiador",
-        //             link: "/",
-        //         },
-        //         {
-        //             name: "Get Down Ropes",
-        //             link: "/",
-        //             subItems: [
-        //                 {
-        //                     name: "16 Feet",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "14 Feet",
-        //                     link: "/",
-        //                 },
-        //             ]
-        //         },
-        //         {
-        //             name: "Key Chains",
-        //             link: "/",
-        //         },
-        //         {
-        //             name: "Jewlery",
-        //             link: "/",
-        //             subItems: [
-        //                 {
-        //                     name: "Bracelets",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "Earrings",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "Sets",
-        //                     link: "/",
-        //                 },
-        //             ]
-        //         },
-        //         {
-        //             name: "Mecates (Mane Horse Hair)",
-        //             link: "/",
-        //             subItems: [
-        //                 {
-        //                     name: "4 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "6 Strands",
-        //                     link: "/",
-        //                 },
-        //                 {
-        //                     name: "8 Strands",
-        //                     link: "/",
-        //                 },
-        //             ]
-        //         },]
-        // },
+
 
     ])
 
