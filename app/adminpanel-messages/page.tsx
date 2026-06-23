4bea9465bef98bd65b9adf5df8c9f4f53ec7b200
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2026-06-23 11:05:56 -0700

    SEO Updates
    Updated admin pages

--- app/adminpanel-messages/page.tsx
+++ app/adminpanel-messages/page.tsx
@@ -4,13 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useEffect } from "react";
 import { getInquiries } from "../utils/apiCalls";
 import Inquiry from "../components/Inquiry";
-import {
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-  useClerk,
-  useUser,
-} from "@clerk/nextjs";
+
 import { useMainStore } from "../utils/global";
 
 const page = (): React.ReactNode => {
@@ -19,27 +13,11 @@ const page = (): React.ReactNode => {
     queryFn: () => getInquiries(),
   });
 
-  const { signOut } = useClerk();
-
-  const { isSignedIn, user } = useUser();
-
   const { setReloadMessages, reloadMessages } = useMainStore();
 
   if (isFetched) {
     console.log(data);
   }
-
-  useEffect(() => {
-    if (
-      isSignedIn &&
-      !(
-        user.id === "user_3C2V9rtJsMvQJUiufTNdnd6bSpf" ||
-        user.id === "user_2yCOu54baLL57crreJsy8nxRKra"
-      )
-    ) {
-      signOut();
-    }
-  }, [user]);
 
   useEffect(() => {
     if (reloadMessages) {
@@ -49,29 +27,24 @@ const page = (): React.ReactNode => {
   }, [reloadMessages]);
   return (
     <>
-      <SignedIn>
-        <div className="flex flex-col items-center p-8 gap-8">
-          {data &&
-            data.data.map((item: any, key: number) => {
-              return (
-                <Inquiry
-                  key={key}
-                  id={item.id}
-                  firstName={item.firstName}
-                  lastName={item.lastName}
-                  email={item.email}
-                  subject={item.subject}
-                  message={item.message}
-                  phoneNumber={item.phoneNumber}
-                  date={item.date}
-                ></Inquiry>
-              );
-            })}
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn></RedirectToSignIn>
-      </SignedOut>
+      <div className="flex flex-col items-center p-8 gap-8">
+        {data &&
+          data.data.map((item: any, key: number) => {
+            return (
+              <Inquiry
+                key={key}
+                id={item.id}
+                firstName={item.firstName}
+                lastName={item.lastName}
+                email={item.email}
+                subject={item.subject}
+                message={item.message}
+                phoneNumber={item.phoneNumber}
+                date={item.date}
+              ></Inquiry>
+            );
+          })}
+      </div>
     </>
   );
 };
