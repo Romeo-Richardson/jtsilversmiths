93f8254ef56210c92de89aa5480e4f512cb9cae0
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2026-06-23 11:46:39 -0700

    Update page.tsx

--- app/adminpanel-delete/page.tsx
+++ app/adminpanel-delete/page.tsx
@@ -1,27 +1,10 @@
 "use client";
 
-import { useClerk, useUser } from "@clerk/nextjs";
 import axios from "axios";
 import React, { useEffect } from "react";
 import toast from "react-hot-toast";
 
 const page = (): React.ReactNode => {
-  const { signOut } = useClerk();
-
-  const { isSignedIn, user } = useUser();
-
-  useEffect(() => {
-    if (
-      isSignedIn &&
-      !(
-        user.id === "user_3C2V9rtJsMvQJUiufTNdnd6bSpf" ||
-        user.id === "user_2yCOu54baLL57crreJsy8nxRKra"
-      )
-    ) {
-      signOut();
-    }
-  }, [user]);
-
   const deleteItem = async (name: FormDataEntryValue | null) => {
     const { data } = await axios.post("/api/delete-item", { name });
     data ? console.log(data) : console.log("Failed to delete item");
