cf0b4685efa594310c20239f20d0cf9e3fe844d6
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2026-08-18 09:27:56 -0700

    Update apiCalls.ts

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -1,14 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getItems = async () => {
-    const { data } = await axios.get("/api/get-items")
-    return data
-}
+  const { data } = await axios.get(
+    "https://www.jtsilversmiths.com/api/get-items",
+  );
+  return data;
+};
 
 export const getInquiries = async () => {
-    const { data } = await axios.get("/api/get-inquiries")
-    if (!data.success) {
-        console.log("Failed to get Inquiries")
-    }
-    return data
-}
+  const { data } = await axios.get("/api/get-inquiries");
+  if (!data.success) {
+    console.log("Failed to get Inquiries");
+  }
+  return data;
+};
