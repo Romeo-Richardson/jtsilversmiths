b4f40ff61e9a3c265ca8e734f12a9f4e27c2e929
Romeo-Richardson <111822421+Romeo-Richardson@users.noreply.github.com>
2026-08-18 19:30:57 -0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import { Metadata, MetadataRoute } from "next";
 import { getItems } from "./utils/apiCalls";
 import { items } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseURL = "https://www.jtsilversmiths.com";
   const products = await getItems();
